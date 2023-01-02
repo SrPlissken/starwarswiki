ed5c6dd555effb4134cfd3832b73e331980a1e8d
Victor Melcon <victormel87@gmail.com>
2023-01-02 12:58:42 +0100

    Initial Commit

---
+++ StarWarsWiki/StarWarsWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsWikiApp.swift
+//  StarWarsWiki
+//
+//  Created by Victor Melcon Diez on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
